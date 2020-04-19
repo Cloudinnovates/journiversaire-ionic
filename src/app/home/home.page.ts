ee4974dd5819122cad3a4650e5282ee64038fcfc
Cedric L'homme <public@l-homme.com>
2020-04-20 01:06:53 +0200

    Add calculation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import add from 'date-fns/add';
+import format from 'date-fns/format';
+import formatDistance from 'date-fns/formatDistance';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +11,28 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   date: string;
+  birthdays = [];
 
   constructor() { }
 
   onChange() {
-
+    this.birthdays = [];
+    const d = new Date(this.date);
+    for (let index = 1; index < 46; index++) {
+      const nextDate = add(d, {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: index * 1000,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      });
+      this.birthdays.push(
+        index + "k: " +
+        format(nextDate, "PPPP") +
+        " (" + formatDistance(nextDate, d) + ")");
+    }
   }
 
 }
